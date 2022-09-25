87efb80ddcefc48c775a18386ccccaab1ff891a3
David León Vilca <datevid@gmail.com>
2022-09-25 03:02:32 -0500

    Add files via upload

---
+++ 16.Navigation1/main.py
@@ -0,0 +1,25 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+
+from kivymd.app import MDApp
+from kivymd.uix.scrollview import MDScrollView
+
+class ContentNavigationDrawer(Screen):
+    #screen_manager = ObjectProperty()
+    #nav_drawer = ObjectProperty()
+    pass
+
+class InterfazMain(ScreenManager):
+    pass
+
+class Example(MDApp):
+    def build(self):
+        #self.theme_cls.primary_palette = "Orange"
+        #self.theme_cls.theme_style = "Dark"
+        #return Builder.load_file("main.kv")
+        screen = Builder.load_file("main.kv")
+        return InterfazMain();
+
+
+Example().run()
